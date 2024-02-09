d8ba41ea0a9d550554a7259a0a3ac23e7401bcca
Yogi Spice <caseynnewman@yahoo.com>
2024-02-08 21:50:23 -0500

    initial commit, setting up server and routes

---
+++ routes/notes.js
@@ -0,0 +1,11 @@
+const notesRouter = require('express').Router()
+
+notesRouter.get('/', (req, res) => {
+    console.log('Something')
+})
+
+notesRouter.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+module.exports = notesRouter
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3113
+
+const app = express()
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const api = require('./routes/index')
+
+app.use('/api', api)
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+)
