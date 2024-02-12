08e3cd7d6367a30aa87992540752a0431136fd74
Yogi Spice <caseynnewman@yahoo.com>
2024-02-12 11:51:47 -0500

    working on delete request

--- server.js
+++ server.js
@@ -4,25 +4,29 @@ const path = require('path');
 
 const PORT = 3113
 
-const app = express()
+const app = express();
 
 app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const api = require('./routes/index')
+const api = require('./routes/index');
 
-app.use('/api', api)
+app.use('/api', api);
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+});
 
-app.listen(PORT, () =>
+app.delete('/api/notes/:id', (req, res) => {
+    console.log('request to delete file received')
+});
+
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
-)
+});
